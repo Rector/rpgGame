b7d28c0612faf2c769dfc9b3f54bc71531f6a9d9
kirill <kirill261989@mail.ru>
2020-11-13 17:27:02 +0400

    Initial commit

---
+++ desktop/src/rpg/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package rpg.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import rpg.game.RPGGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
+		new LwjglApplication(new RPGGame(), config);
+	}
+}
