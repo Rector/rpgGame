de16275d63ca5f8eea8098a50a374e3141809d28
kirill <kirill261989@mail.ru>
2020-11-20 12:07:19 +0400

    add 04 Task Lesson

---
+++ core/src/rpg/game/BattleCalc.java
@@ -0,0 +1,23 @@
+package rpg.game;
+
+import com.badlogic.gdx.math.MathUtils;
+import rpg.game.units.Unit;
+
+public class BattleCalc {
+    public static int attack(Unit attacker, Unit target) {
+        int out = attacker.getDamage();
+        out -= target.getDefence();
+        if (out < 0) {
+            out = 0;
+        }
+        return out;
+    }
+
+    public static int checkCounterAttack(Unit attacker, Unit target) {
+        if (MathUtils.random() < 0.2f) {
+            int amount = attack(target, attacker);
+            return amount;
+        }
+        return 0;
+    }
+}
