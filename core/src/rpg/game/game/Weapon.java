2a1bd73bc70217adc36654ce7e07d9b889840b70
kirill <kirill261989@mail.ru>
2020-11-29 18:19:03 +0400

    add 07 Task Lesson (base)

---
+++ core/src/rpg/game/game/Weapon.java
@@ -0,0 +1,21 @@
+package rpg.game.game;
+
+import lombok.Data;
+
+@Data
+public class Weapon {
+    public enum Type {
+        SPEAR, SWORD, MACE, AXE, BOW
+    }
+
+    Type type;
+    int damage;
+    int radius;
+
+    public Weapon(Type type, int damage, int radius) {
+        this.type = type;
+        this.damage = damage;
+        this.radius = radius;
+    }
+}
+
